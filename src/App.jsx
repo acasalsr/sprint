3c279d96062153f97d5edc5be09d4864fafc51a4
andreu casals <acasalsr@gmail.com>
2022-10-24 21:47:38 +0200

    json connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Data from "./data/data.json";
 
 function App() {
   return (
     <>
       <h1 className=" text-lg text-cyan-800">hola</h1>
+      {Data.map((dataJson) => {
+        return (
+          <>
+            <p>{dataJson.name}</p>
+            <img src={dataJson.src} alt={dataJson.name} />
+            <br />
+          </>
+        );
+      })}
     </>
   );
 }
